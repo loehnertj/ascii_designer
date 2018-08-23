867e9f3012e52d800c5e9762e0f7b7bd8c709582
Johannes Loehnert <loehnert.kde@gmx.net>
2018-08-23 17:57:40 +0200

    add all the controls except multiline. No binding so far

--- ascii_designer/toolkit_tk.py
+++ ascii_designer/toolkit_tk.py
@@ -1,21 +1,43 @@
 '''This is a construction site...''' 
 
 import tkinter as tk
+from tkinter import font
+from tkinter import ttk
 from .toolkit import ToolkitBase
+
+#ttk = tk
+
 
 __all__ = [
     'ToolkitTk',
 ]
 
 class ToolkitTk(ToolkitBase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        r = self.root
+        s = ttk.Style()
+        # set tkinter widget fonts
+        #s.configure('', font=self._sane_font)
+        
+        self._radiobutton_var = tk.StringVar()
+        
     # widget generators
     @property
     def root(self):
         '''return the root widget'''
         if not self._root:
             self._root = tk.Tk()
+            self._root.tk.call('tk', 'scaling', 2.0)
+            self._root.option_add('*Font', self._sane_font)
         return self._root
         
+    @property
+    def _sane_font(self):
+        if not hasattr(self, '_sane_font_cached'):
+            self._sane_font_cached = font.Font(family='Helvetica', size=12)
+        return self._sane_font_cached
+    
     @root.setter
     def root(self, val):
         self._root = val
@@ -75,33 +97,61 @@ class ToolkitTk(ToolkitBase):
         
     def label(self, id=None, label_id=None, text=None):
         '''label'''
-        return tk.Label(self.root, text=(text or '').strip())
+        return tk.Label(self.root, name=id, text=(text or '').strip())
         
     def button(self, id=None, text=None):
         '''button'''
-        return tk.Button(self.root, text=(text or '').strip())
+        return tk.Button(self.root, name=id, text=(text or '').strip())
     
     def textbox(self, id=None, text=None):
         '''single-line text entry box'''
-        return tk.Entry(self.root, text=(text or '').strip())
+        return tk.Entry(self.root, name=id, text=(text or '').strip())
     
     def multiline(self, id=None, text=None):
         '''not supported'''
-    
+        
     def dropdown(self, id=None, text=None, values=None):
-        '''dropdown box; values is the raw string between the parens. Only preset choices allowed.'''
-        choices = [v.strip() for v in (values or '').split(',') if v.strip()]
-        return tk.OptionMenu(self.root, '',(text or '').strip(), *choices)
+        return self._dropdown(id, text, values, False)
     
     def combo(self, id=None, text=None, values=None):
-        '''not supported'''
-        
+        return self._dropdown(id, text, values, True)
+    
+    def _dropdown(self, id=None, text=None, values=None, editable=False):
+        '''dropdown box; values is the raw string between the parens. Only preset choices allowed.'''
+        choices = [v.strip() for v in (values or '').split(',') if v.strip()]
+        tv = tk.StringVar()
+        tv.set((text or '').strip())
+        cbo = ttk.Combobox(self.root, name=id, values=choices, textvariable=tv, state='normal' if editable else 'readonly')
+        return cbo
+    
     def option(self, id=None, text=None, checked=None):
         '''Option button. Prefix 'O' for unchecked, '0' for checked.'''
+        rb = tk.Radiobutton(self.root,
+                  text=(text or '').strip(),
+                  variable=self._radiobutton_var, 
+                  name=id,
+                  value=id,
+        )
+        if checked:
+            self._radiobutton_var.set(id)
+        return rb
+    
     def checkbox(self, id=None, text=None, checked=None):
         '''Checkbox'''
+        v = tk.IntVar()
+        cb = tk.Checkbutton(
+            self.root,
+            text=(text or '').strip(),
+            name=id,
+            variable=v
+            )
+        if checked:
+            v.set(1)
+        return cb
     def slider(self, id=None, min=None, max=None):
         '''slider, integer values, from min to max'''
+        return tk.Scale(self.root, name=id, from_=min, to=max, orient=tk.HORIZONTAL)
+    
     def external(self, id=None):
         '''external reference'''
         return getattr(self._external_reference_provider, id)
