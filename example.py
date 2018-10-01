3a03fb48f202596c2fa1eeba6800773caac938a1
Johannes Loehnert <loehnert.kde@gmx.net>
2018-10-01 21:56:11 +0200

    update README

---
+++ example.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+'''Minimalistic but full working example form.'''
+
+from ascii_designer import AutoFrame
+
+class TextTransformer(AutoFrame):
+    frame_body='''
+                            |              |
+        Text to transform:   [ Text_      ]
+        
+        Select transformation:
+        
+        (x) Uppercase
+        ( ) Lowercase
+        ( ) Title-case
+        
+            [ OK ]            [ Cancel ]~
+
+    '''
+    def ok(self):
+        text = self.text
+        if self.uppercase:
+            text = text.upper()
+        elif self.lowercase:
+            text = text.lower()
+        elif self.titlecase:
+            text = text.title()
+        print(text)
+        self.close()
+        
+    def cancel(self):
+        self.close()
+        
+if __name__ == '__main__':
+    TextTransformer().frame_show()
+
