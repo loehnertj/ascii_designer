65719622a13624962c0603663ce38f5dd6f5bc11
Johannes Loehnert <loehnert.kde@gmx.net>
2022-04-09 09:42:06 +0200

    Add 3rd challenge

---
+++ 7gui/3_flight_booker.py
@@ -0,0 +1,93 @@
+from datetime import date, datetime
+import tkinter.ttk as ttk
+from tkinter.messagebox import showinfo
+from ascii_designer import set_toolkit, AutoFrame, Invalid
+
+def str2date(s):
+    return datetime.strptime(s, "%d.%m.%Y").date()
+
+def date2str(dt):
+    return dt.strftime("%d.%m.%Y")
+
+class FlightBooker(AutoFrame):
+    f_body = """
+        |   <->
+         [kind:  v]
+         [t1: _   ]
+         [t2: _   ] 
+          [ Book ]
+    """
+    f_option_tk_autovalidate = True
+
+    def f_on_build(self):
+        self["kind"]["values"] = ["one-way flight", "return flight"]
+        # XXX: This is not the right place for style setup. TBD: make it
+        # possible to extend the styles in set_toolkit, without losing the
+        # defaults.
+        style = ttk.Style()
+        style.map(
+            "TEntry", 
+            # Order matters: earlier entries take precedence.
+            fieldbackground=[("disabled", "#d0d0d0"), ("invalid", "red")],
+            foreground=[("disabled", "#808080"), ("invalid", "black")]
+        )
+        style.map(
+            "TButton",
+            foreground=[("disabled", "#808080")]
+        )
+        self["t1"].variable.convert = str2date
+        self["t2"].variable.convert = str2date
+        self["t1"].variable.convert_set = date2str
+        self["t2"].variable.convert_set = date2str
+
+        self.kind = "one-way flight"
+        self.t1 = self.t2 = datetime.now().date()
+        # initial ctl update
+        self.on_kind(self.kind)
+
+    @property
+    def booking_possible(self):
+        """True if all constraints are met."""
+        dt1 = self.t1
+        dt2 = self.t2
+        if dt1 is Invalid:
+            return False
+        if self.kind == "return flight":
+            if dt2 is Invalid:
+                return False
+            if dt2 < dt1:
+                return False
+        return True
+
+    def _update_book(self):
+        self["book"].state(["!disabled"] if self.booking_possible else ["disabled"])
+
+    def on_kind(self, kind):
+        """Enable/disable T2, then check constraints."""
+        self["t2"].state(["!disabled"] if kind == "return flight" else ["disabled"])
+        self._update_book()
+
+    def on_t1(self, dt1):
+        """Recheck constraints."""
+        self._update_book()
+
+    def on_t2(self, dt2):
+        """Recheck constraints."""
+        self._update_book()
+
+    def book(self):
+        """Execute booking"""
+        dt1 = self.t1
+        if self.kind == "one-way flight":
+            message = f"You have booked a one-way flight on {date2str(dt1)}."
+        elif self.kind == "return flight":
+            dt2 = self.t2
+            message = f"You have booked a return flight from {date2str(dt1)} to {date2str(dt2)}."
+        else:
+            # Safeguard
+            raise ValueError("Unexpected flight kind")
+        showinfo("Booking successfull", message)
+
+if __name__ == "__main__":
+    set_toolkit("ttk")
+    FlightBooker().f_show()
