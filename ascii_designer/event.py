11a453cde9357d5a3b7a8231e683f3b196596906
Johannes Loehnert <loehnert.kde@gmx.net>
2023-06-11 00:07:28 +0200

    Supercharged Event system Can use @event decorator now, to get full signature checking and docstrings.

--- ascii_designer/event.py
+++ ascii_designer/event.py
@@ -1,46 +1,176 @@
-'''Provides an event dispatcher class.
-'''
-
-# Bells and whistles that could be added:
-# - signature check
-# - description
+"""
+``@event`` decorator for methods, to make them into "subscribable" events.
+"""
 
 ___all__ = [
-    'EventSource',
-    'CancelEvent',
-
+    "event",
+    "Event",
+    "CancelEvent",
 ]
 
+import inspect
+from functools import wraps
+
+try:
+    from typing import Self
+except ImportError:
+    Self = None
+
+from weakref import WeakValueDictionary
+# FIXME: Use weakref for listeners as well
+
+
 class CancelEvent(Exception):
-    '''Raise this in an event handler to inhibit all further processing.'''
+    """Raise this in an event handler to inhibit all further processing."""
 
-class EventSource:
-    '''Event dispatcher class
-    
-    You can register / unregister handlers via ``+=`` and ``-=`` methods.
 
-    Handlers *may* return a result. If multiple handlers return results, last
-    one is returned to the event's source.
-    '''
-    def __init__(self):
-        self._handlers = []
+class Event:
+    """Event descriptor
 
-    def __iadd__(self, handler):
-        self._handlers.append(handler)
-        return self
+    - call to notify all listeners
+    - exception in a listener will stop processing and be raised at the call
+    (Trigger) site. Event-triggering function should handle any exceptions
+    thrown at it.
+    """
 
-    def __isub__(self, handler):
-        self._handlers.remove(handler)
-        return self
+    def __init__(self, prototype=None, by_name=True):
+        self._prototype = prototype
+        self._by_name = by_name
+        self._listeners = []
+        if self._prototype:
+            sig: inspect.Signature = inspect.signature(self._prototype)
+            P = inspect.Parameter
+            if any(p.default is not P.empty for p in sig.parameters.values()):
+                raise TypeError("Default values are forbidden for events")
+            if any(
+                p.kind in (P.VAR_POSITIONAL, P.VAR_KEYWORD)
+                for p in sig.parameters.values()
+            ):
+                raise TypeError("*args and **kwargs are forbidden for events")
+            self._self_arg = "self" in sig.parameters
+            self._argnames = [
+                p.name for p in sig.parameters.values() if p.name != "self"
+            ]
+        else:
+            self._self_arg = False
+            self._argnames = []
+        self._is_bound = False
+        self._bound_copies = WeakValueDictionary()
+
+    def __get__(self, instance, owner):
+        # Copy the event for each instance, so that that each instance
+        # has its private list of listeners.
+        if instance is None:
+            return self
+        key = id(instance)
+        try:
+            return self._bound_copies[key]
+        except KeyError:
+            ev = Event(self._prototype, self._by_name)
+            ev._is_bound = True
+            self._bound_copies[key] = ev
+            return ev
 
     def __call__(self, *args, **kwargs):
-        '''Trigger the event. To be called by the event's owner.'''
-        all_result = None
-        for handler in self._handlers:
+        results = []
+        if self._prototype:
+            # Checks args/kwargs against specificed signature
+            # If the self arg is there, supply it.
+            if self._self_arg:
+                r = self._prototype(None, *args, **kwargs)
+            else:
+                r = self._prototype(*args, **kwargs)
+            if r is not None:
+                results.append((r, self._prototype))
+            # Hide internal call semantics (by-position or by-name) from called
+            # listeners, by normalizing to the specified behavior.
+            if self._by_name:
+                # convert args to kwargs
+                kwargs1 = {name: val for name, val in zip(self._argnames, args)}
+                kwargs = kwargs.copy()
+                kwargs.update(kwargs1)
+                args = []
+            else:
+                # convert kwargs to args
+                args = list(args)
+                for name in self._argnames[len(args) :]:
+                    args.append(kwargs[name])
+                kwargs = {}
+        # === Call each listener ===
+        for listener in self._listeners:
             try:
-                result = handler(*args, **kwargs)
+                r = listener(*args, **kwargs)
+                if r is not None:
+                    results.append((r, listener))
             except CancelEvent:
                 break
-            if result is not None:
-                all_result = result
-        return all_result
+        if len(results) > 1:
+            raise ValueError("Got more than one event result: %r" % (results,))
+        elif len(results) == 1:
+            return results[0][0]
+        else:
+            return None
+
+    # TODO: Signature of listener
+    def __iadd__(self, listener) -> Self:
+        if self._listeners is None:
+            raise TypeError("Cannot add listener to unbound event")
+        self._listeners.append(listener)
+        return self
+
+    def __isub__(self, listener) -> Self:
+        if self._listeners is None:
+            raise TypeError("Cannot remove listener from unbound event")
+        self._listeners.remove(listener)
+        return self
+
+    def __str__(self):
+        if not self._prototype:
+            return "<Event>"
+        names = ", ".join(self._argnames)
+        prefix = "Bound" if self._is_bound else "Unbound"
+        return f"<{prefix} Event {self._prototype.__qualname__}({names})>"
+
+    __repr__ = __str__
+
+
+# legacy alias
+EventSource = Event
+
+
+def event(prototype=None, by_name=True):
+    """Decorator that turns a method into an Event.
+
+    Event handlers can be subscribed/unsubscribed using the ``+=`` and ``-=`` operators.
+
+    Invocation of an event methods makes it call its associated handlers.
+
+    The event method is expected to not have any body except for a docstring.
+
+    Arguments with default values are forbidden, since their meaning would be
+    ambiguous for the consumer.
+
+    ``by_name`` controls whether arguments are passed to the handlers as positional
+    arguments (args) or as named arguments (kwargs). The latter is recommended.
+
+    * ``by_name=True``: Handlers must provide the same argument names as in the signature
+    * ``by_name=False``: Handlers must provide the same argument order as in the singature.
+    """
+    if not prototype:
+        # when called as @decorator(...)
+        return lambda prototype: event(prototype=prototype, by_name=by_name)
+
+    fn_wrapper = Event(prototype, by_name=by_name)
+    fn_wrapper = wraps(prototype)(fn_wrapper)
+    return fn_wrapper
+
+
+# === usage demo ===
+
+
+class HasEvents:
+    """a class that defines an event"""
+
+    @event(by_name=False)
+    def foo(self, arg1: int, boolarg: bool):
+        """Triggered when foo happens."""
---
+++ tests/test_event.py
@@ -0,0 +1,228 @@
+import pytest
+import inspect
+from unittest.mock import Mock
+from ascii_designer.event import Event, EventSource, event, CancelEvent
+
+# TBD:
+# binding to class event:
+# XXX: Expectation? Should invoke both on Cls.foo and instance.foo?
+# XXX: how about subclassing, invoke for all subclasses?
+# How should the self parameter work?
+
+@pytest.fixture
+def ev1():
+    @event
+    def ev1():
+        """Event 1"""
+
+    return ev1
+
+@pytest.fixture
+def ev2():
+    @event
+    def ev2(a: int):
+        """Event 2"""
+
+    return ev2
+
+
+@pytest.fixture
+def Cls():
+    class Cls:
+        @event
+        def ev1(a:int):
+            pass
+
+        @event
+        def ev2(self, a):
+            pass
+
+    return Cls
+
+
+def test_event_source_legacy_alias():
+    assert EventSource is Event
+
+
+def test_event_legacy():
+    """Can define and use Event() without prototype (legacy behavior)
+
+    Will accept and pass on any arguments as given.
+    """
+    ev = Event()
+    ev += (m := Mock())
+    ev()
+    m.assert_called_with()
+    ev(1, x=2)
+    m.assert_called_with(1, x=2)
+
+
+
+def test__event_docstring(ev1):
+    """Docstring is passed through"""
+    assert ev1.__doc__ == "Event 1"
+
+
+# not working yet, since Event is actually a class
+@pytest.mark.xfail
+def test_event_signature():
+    """parameter names and annotations are passed"""
+
+    @event
+    def ev(a: int):
+        pass
+
+    sig = inspect.Signature(ev)
+    assert len(sig.parameters) == 1
+    p = sig.parameters["a"]
+    assert p.name == "a"
+    assert p.annotation is int
+
+def test_event_signature_check(ev2):
+    """Calling event with bad parameters raises TypeError"""
+    ev=ev2
+
+    with pytest.raises(TypeError): # a is missing
+        ev()
+    with pytest.raises(TypeError): # b is extra
+        ev(1, b=1)
+    with pytest.raises(TypeError): # 2, 3 are extra
+        ev(1, 2, 3)
+    # This does not constitute an error, since type hints are not enforced.
+    ev("not an integer")
+    # Can use kwarg syntax as well
+    ev(a=1)
+
+@pytest.mark.parametrize("handler,iserror", [
+    (lambda a, b:None, True),
+    (lambda a, b=1:None, False),
+    # By default, kwargs are passed
+    (lambda *args: None, True),
+    (lambda **kwargs:None, False),
+    # a is missing
+    (lambda b, c: None, True),
+])
+def test_event_handler_signature(ev2, handler, iserror):
+    """Event *handler* signature is not enforced when subscribing.
+    
+    Handler with wrong signature will raise TypeError when invoked.
+
+    Handler may specify additional params with default value, and/or catch
+    values in ``**kwargs``.
+    """
+    # Never fails
+    ev2 += handler 
+    if iserror:
+        with pytest.raises(TypeError):
+            ev2(1)
+    else:
+        ev2(1)
+
+
+
+def test_event_str(ev1, Cls):
+    o = Cls()
+    assert str(Event()) == "<Event>"
+    assert str(ev1) == "<Unbound Event ev1.<locals>.ev1()>"
+    assert str(Cls.ev1) == "<Unbound Event Cls.<locals>.Cls.ev1(a)>"
+    assert str(Cls.ev2) == "<Unbound Event Cls.<locals>.Cls.ev2(a)>"
+    assert str(o.ev1) == "<Bound Event Cls.<locals>.Cls.ev1(a)>"
+    assert str(o.ev2) == "<Bound Event Cls.<locals>.Cls.ev2(a)>"
+
+
+def test_event_no_kwargs():
+    """Cannot define event with kwargs"""
+    with pytest.raises(TypeError):
+
+        @event
+        def ev(kwarg=1):
+            pass
+
+
+def test_event_no_kwargs():
+    """Cannot define event with *args"""
+    with pytest.raises(TypeError):
+
+        @event
+        def ev(*args):
+            pass
+
+
+def test_event_no_kwargs():
+    """Cannot define event with **kwargs"""
+    with pytest.raises(TypeError):
+
+        @event
+        def ev(**kwargs):
+            pass
+
+
+def test_module_event_fire(ev1):
+    """Module-level event notification works in principle"""
+    ev1 += (m := Mock())
+    ev1()
+    m.assert_called_with()
+
+
+def test_event_namedargs():
+    """By default, args are passed as named args"""
+
+    @event
+    def ev(a, b):
+        pass
+
+    ev += (m := Mock())
+    ev(1, 2)
+    m.assert_called_with(a=1, b=2)
+
+
+def test_event_posargs():
+    """If configured so, args are passed positional"""
+
+    @event(by_name=False)
+    def ev(a, b):
+        pass
+
+    ev += (m := Mock())
+    ev(1, 2)
+    m.assert_called_with(1, 2)
+
+
+def test_class_event_fire(Cls):
+    """Object-level event notification works in principle"""
+    o = Cls()
+    o.ev1 += (m := Mock())
+    o.ev1(1)
+    m.assert_called_with(a=1)
+
+
+def test_class_handler_separation(Cls):
+    """Instances have separate handler lists"""
+    o1 = Cls()
+    o2 = Cls()
+    o1.ev1 += (m1 := Mock())
+    o2.ev1 += (m2 := Mock())
+    o1.ev1(1)
+    o2.ev1(2)
+    m1.assert_called_once_with(a=1)
+    m2.assert_called_once_with(a=2)
+
+def test_class_self_handling(Cls):
+    """self argument CAN be there but is ignored."""
+    o = Cls()
+    Cls.ev2 += (m1:=Mock())
+    o.ev2 += (m2:=Mock())
+
+    Cls.ev2(1)
+    o.ev2(2)
+
+    m1.assert_called_once_with(a=1)
+    m2.assert_called_once_with(a=2)
+
+# Not yet implemented
+@pytest.mark.xfail
+def test_class_call_with_self(Cls):
+    Cls.ev2 += (m:=Mock())
+    o = Cls()
+    Cls.ev2(o, 3)
+    # XXX WHat do we expect here!?
